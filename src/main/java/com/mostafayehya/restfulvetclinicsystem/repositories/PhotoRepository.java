e015b9ed8a61832e0061293a3e635e10f6aa9e3f
MostafaYahia <mostafayehyax23@gmail.com>
2020-09-21 22:05:23 +0200

    Configure H2 in cache database url and path

--- src/main/java/com/mostafayehya/restfulvetclinicsystem/repositories/PhotoRepository.java
+++ src/main/java/com/mostafayehya/restfulvetclinicsystem/repositories/PhotoRepository.java
@@ -1,7 +1,5 @@
 package com.mostafayehya.restfulvetclinicsystem.repositories;
 
-import com.mostafayehya.restfulvetclinicsystem.domain.Photo;
-import org.springframework.data.repository.CrudRepository;
 
 public interface PhotoRepository  {
 }
