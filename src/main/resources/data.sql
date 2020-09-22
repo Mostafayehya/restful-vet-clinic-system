4341659646ada01a2922b37c3e69a3adcc3b752b
MostafaYahia <mostafayehyax23@gmail.com>
2020-09-22 16:19:56 +0200

    Add SQL scripts to add dummy clinics to DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,16 @@
 INSERT INTO owners (name,gender,email,phone,address)
- VALUES ('Mostafa Yehya','Male','mostafayehyax23@gmail.com','010574813748'
-    ,'Nozha, Cairo');
+VALUES ('Mostafa Yehya','Male','mostafayehyax23@gmail.com','010574813748','Nozha, Cairo');
 
-INSERT INTO pets (name,gender,date_of_birth,animal_kind,photo_url,weight
-                 ,owner_id)
- VALUES ('Bolt','Male','20/5/2019','Dog'
-    ,'https://images.app.goo.gl/VNfz6Wn453zbcova9','10 KG',1);
+INSERT INTO pets (name,gender,date_of_birth,animal_kind,photo_url,weight,owner_id)
+VALUES ('Bolt','Male','20-5/-019','Dog','https://images.app.goo.gl/VNfz6Wn453zbcova9','10 KG',1);
 
-INSERT INTO pets (name,gender,date_of_birth,animal_kind,photo_url,weight
-                 ,owner_id)
- VALUES ('Possy','Female','4/11/2018','Cat'
-    ,'https://images.app.goo.gl/xWuBVAoTZkaa5fL77','5 KG',1);
+INSERT INTO pets (name,gender,date_of_birth,animal_kind,photo_url,weight,owner_id)
+VALUES ('Possy','Female','4-11-2018','Cat','https://images.app.goo.gl/xWuBVAoTZkaa5fL77','5 KG',1);
+
+INSERT INTO clinics (name,address,phone,working_days,email,social_networks)
+VALUES ('Dog Clinic','Madinit Nasr',01027485123,'Monday, Tuesday 9 am to 2 pm','dogclinic@vetcompany.com','www.dogclinic.com');
+
+INSERT INTO clinics (name,address,phone,working_days,email,social_networks)
+VALUES ('Cat Clinic','Haram',011564185,'Friday and Saturday 10 am to 4 pm','catclinic@vetcompany.com','www.catclinic.com');
 
 
