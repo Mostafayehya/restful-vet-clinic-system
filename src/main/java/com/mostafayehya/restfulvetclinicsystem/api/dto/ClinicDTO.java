56e4300b22f00a3db39fdec360342a43c07a1e2d
MostafaYahia <mostafayehyax23@gmail.com>
2020-09-22 09:35:52 +0200

    Add Clinic and Clinic DTO

--- src/main/java/com/mostafayehya/restfulvetclinicsystem/api/dto/ClinicDTO.java
+++ src/main/java/com/mostafayehya/restfulvetclinicsystem/api/dto/ClinicDTO.java
@@ -1,29 +1,24 @@
 package com.mostafayehya.restfulvetclinicsystem.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mostafayehya.restfulvetclinicsystem.domain.Pet;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.util.HashSet;
-import java.util.Set;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class OwnerDTO {
+public class ClinicDTO {
 
     private String name;
-    private String gender;
-    private String email;
-    private String phone;
     private String address;
+    private String phone;
 
-    @JsonProperty("pet_list")
-    Set<PetDTO> petList = new HashSet<>();
+    @JsonProperty("working_days_and_hours")
+    private String workingDaysAndHours;
 
-
+    private String email;
+    private String urls;
 }
--- src/main/java/com/mostafayehya/restfulvetclinicsystem/domain/Clinic.java
+++ src/main/java/com/mostafayehya/restfulvetclinicsystem/domain/Clinic.java
@@ -1,4 +1,38 @@
 package com.mostafayehya.restfulvetclinicsystem.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "clinics")
 public class Clinic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "working_days")
+    private String workingDaysAndHours; // Using String for simplicity
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "social_networks")
+    private String urls; // Possible URL entity
+
 }
